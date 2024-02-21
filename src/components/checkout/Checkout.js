e7efd18b32cee542737b621adb005af14326a04a
Wardah Jabeen <jabeen@usc.edu>
2024-02-21 15:25:54 -0800

    fixed some css for smaller screen

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -13,13 +13,15 @@ const Checkout = () => {
             <div className='checkout__header'>
                 <Header />
             </div>
+            <img
+                className='checkout__ad'
+                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                alt=""
+            />
             <div className='checkout__body'>
+
                 <div className='checkout__left'>
-                    <img
-                        className='checkout__ad'
-                        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                        alt=""
-                    />
+
                     {basket?.length === 0 ? (
                         <div>
                             <h2>Your Shopping Basket is empty</h2>
